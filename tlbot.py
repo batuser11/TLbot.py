e79ce725b4b37592d12932bab3ab1f3776b4a918
batuser11 <167933883+batuser11@users.noreply.github.com>
2024-04-26 03:09:48 +0300

    Update tlbot.py

--- tlbot.py
+++ tlbot.py
@@ -1,12 +1,12 @@
 import telebot
 from decouple import config
 
-BOT_token = config('BOT_TOKEN')
+BOT_TOKEN = config('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commads=["hello", "help"])
 def send_hello_message(msg):
-    bot.reply_to(msg,"hello to my bot what you whant?")
+    bot.reply_to(msg, "hello to my bot what you whant?")
     
 bot.polling()

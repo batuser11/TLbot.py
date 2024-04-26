f4b61764369171f90b9e5e36eafe62379cdd980a
batuser11 <167933883+batuser11@users.noreply.github.com>
2024-04-26 03:02:45 +0300

    Update tlbot.py

--- tlbot.py
+++ tlbot.py
@@ -7,6 +7,6 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commads=["hello","help"])
 def send_hello_message(msg):
-    bot.reply_to(msg, "hello to my bot what you whant?")
+    bot.reply_to(msg,"hello to my bot what you whant?")
     
 bot.polling()
